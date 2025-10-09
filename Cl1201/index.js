6d187cb149a3ae53ae0453d0c9aae3ecf69d1fe4
chelsey <c@mail.com>
2025-10-09 16:01:45 +0800

    Added more text and a button

---
+++ Cl1201/index.js
@@ -0,0 +1,10 @@
+function Clicked() {
+    console.log ("Click")
+    var Click = 3
+    console.log(Click) 
+    Click = Click + 100
+    Click *= 39
+    // 1 + 1
+    // Click + 1
+    console.log(Click) 
+}
