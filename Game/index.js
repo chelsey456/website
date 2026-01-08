8bd20699d87f3a0135d7133acc83d29b8c167f4a
chelsey456 <zav11c00@gmail.com>
2026-01-08 16:04:32 +0800

    edited flappy bird game

--- Game/index.js
+++ Game/index.js
@@ -1,7 +1,31 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+let birdimg;
+
+let bird = {
+	x: birdX,
+	y: birdY,
+	width: birdWidth,
+	height: birdHeight
 }
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+
+window.onload = function () {
+	board = document.getElementById("board");
+	board.height = boardHeight;
+	board.width = boardWidth;
+	context = board.getContext("2d");
+
+	context.fillstyle = "green";
+	context.fillrect(bird.x, bird.y, bird.width, bird.height);
+	
+	birdimg = new Image();
+	birdimg.src = "https://www.pngall.com/wp-content/uploads/15/Flappy-Bird-PNG-Photos.png"
+}
